1823dd4df684a8a369743243656da732ea3f27d7
Reham <r_nour30@yahoo.com>
2023-09-07 16:50:37 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  *Description: puts function is for print otuput
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Discription: This code is for printf function with c programming language.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
