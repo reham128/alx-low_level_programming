e7af924d5806a6ccb8aa5b0b935b5986c1b192d4
Reham <r_nour30@yahoo.com>
2023-09-07 16:09:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
-#includ <stdio.h>
-int main(){
- 	puts("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Description: puts function is for print otuput
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
