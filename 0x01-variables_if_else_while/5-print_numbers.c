0e7d9df6da9552acb93227b4d43597604fa3e35c
Reham <r_nour30@yahoo.com>
2023-09-11 15:53:53 -0700

    for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription: Positive anything is better than negative nothing.
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	return (0);
+}
