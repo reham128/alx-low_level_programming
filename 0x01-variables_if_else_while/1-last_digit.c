6920f438e6082c31e6877715143b7b6fe5ac2bf4
Reham <r_nour30@yahoo.com>
2023-09-11 14:17:36 -0700

    When a number is divided by 10, the remainder is always the last digit of the number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_Dig = n % 10;
 	if (last_Dig > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_Digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_Dig);
 	else if (last_Dig == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last_Dig);
 	else
