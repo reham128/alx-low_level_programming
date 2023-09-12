48252270521e276ff9cc900aeb1a723f9e2278d4
Reham <r_nour30@yahoo.com>
2023-09-11 17:42:40 -0700

    putchar day

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription: Positive anything is better than negative nothing.
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int hex;
+
+	for (hex = '0'; hex < 10; hex++)
+	{
+		putchar(hex);
+	}
+	for (hex = 'a'; hex <= 'f'; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+	return (0);
+}
