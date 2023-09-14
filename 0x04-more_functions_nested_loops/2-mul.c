e1e62ed55781d0fb5ca432412724b5997fa5f876
Reham <r_nour30@yahoo.com>
2023-09-14 15:34:14 -0700

    multiplcation

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -  function that multiplies two integers.
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return product.
+ */
+int mul(int a, int b)
+{
+	product = a * b;
+	return (product);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
