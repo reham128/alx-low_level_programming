1da5aca37354ec428f1bfbeb2d3f5735fa5f3a9d
Reham <r_nour30@yahoo.com>
2023-09-14 19:52:32 -0700

    fuzz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,18 +15,23 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0)
-			printf("Fizz");
-		else if (n % 5 == 0)
-			printf("Buzz");
-		else if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz");
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
 		else
-			printf("%d", n);
-		if (n != 100)
-			prinf(" ");
-		else
-			printf("\n");
+		{
+			printf("%d ", i);
+		}
 	}
+	printf("\n");
 
 	return (0);
 }
