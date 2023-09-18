cf9e298526c202d364279f8ffc3dc85dd8039b21
Reham <r_nour30@yahoo.com>
2023-09-18 16:09:31 -0700

    pp

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,11 +16,15 @@ void puts_half(char *str)
 	int start_index;
 	int i;
 
-	if (length % 2 == 1)
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
 	{
 		start_index = (length - 1) / 2;
 	}
-	for (i = start_index; i < length; i++)
+	for (i = start_index + 1; i < length; i++)
 	{
 		_putchar(str[i]);
 	}
