9997529be2440e723a9f8f14488344c41b4baf97
Reham <r_nour30@yahoo.com>
2023-09-28 05:31:49 -0700

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,9 @@
 */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s >= 0 && *s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
-	else if (*s == 0)
-	{
-		_putchar('\n');
 	}
 }
