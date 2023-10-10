47a926686675aa9f434ad1819a99ae6c9578502f
Reham <r_nour30@yahoo.com>
2023-10-10 06:14:25 -0700

    Bob's Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef LITTLE_DOG_H
+#define LITTLE_DOG_H
+/**
+ * struct dog - a new structure dog
+ *
+ * @name: is the dog name
+ *
+ * @age: is the dog age.
+ *
+ * @owner: is the dog's owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
