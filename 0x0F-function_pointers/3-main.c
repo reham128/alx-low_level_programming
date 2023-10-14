646924d3e0b21428b9a08cccb6ec175b53658b0c
Reham <r_nour30@yahoo.com>
2023-10-13 19:12:54 -0700

    last try

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	y = atoi(argv[3]);
 
 	opFunc = get_op_func(argv[2]);
-	if (opFunc == NULL)
+	if (opFunc == NULL || op[1] !='\0')
 	{
 		printf("Error\n");
 		exit(99);
