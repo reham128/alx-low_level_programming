64e9d51cddc23460dc757fbb2ac677f700694db7
Reham <r_nour30@yahoo.com>
2023-10-14 11:58:36 -0700

    betty check

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,9 +1,11 @@
 #include "3-calc.h"
 /**
  * op_add - to add two integer numbers.
+ * Description: add function.
  * @a: first parameter.
  * @b: second parameter.
- * Return : a + b.
+ *
+ * Return: result value of a + b.
  */
 int op_add(int a, int b)
 {
@@ -13,9 +15,11 @@ int op_add(int a, int b)
 }
 /**
  * op_sub - to subtract two integer numbers.
+ * Description: subtraction function.
  * @a: first parameter.
  * @b: second parameter
- * Return : a - b.
+ *
+ * Return: result  value of a - b.
  */
 int op_sub(int a, int b)
 {
@@ -25,9 +29,11 @@ int op_sub(int a, int b)
 }
 /**
  * op_mul - to multiply two integer numbers.
+ * Description: multiply function.
  * @a: first parameter
  * @b: second parameter
- * Return : a * b.
+ *
+ * Return: result value of a * b.
  */
 int op_mul(int a, int b)
 {
@@ -37,9 +43,11 @@ int op_mul(int a, int b)
 }
 /**
  * op_div - to divide two integer numbers.
+ * Description: divide function.
  * @a: first parameter
  * @b: second parameter
- * Return : a / b.
+ *
+ * Return: result value of a / b.
  */
 int op_div(int a, int b)
 {
@@ -51,7 +59,8 @@ int op_div(int a, int b)
  * op_mod - to finde the reminder of division for two integer numbers.
  * @a: first parameter
  * @b: second parameter
- * Return : a % b.
+ *
+ * Return: result value of a % b.
  */
 int op_mod(int a, int b)
 {
