f7d526f77369702d986d6000a4f9590124eb26bb
Reham <r_nour30@yahoo.com>
2023-10-24 14:50:23 -0700

    node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
  * get_nodeint_at_index -  function that returns the nth node of a listint_t.
  * @head: pointer to the list head.
  * @index: the addres to put the node at it.
- * Return : pointer to the add node.
+ * Return: pointer to the add node.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
