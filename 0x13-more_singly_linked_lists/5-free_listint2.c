69455e96a12404553802da042dbd285554ab2f31
Reham <r_nour30@yahoo.com>
2023-10-24 13:52:18 -0700

    w

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_listint2 - a function that frees a listint_t list.
+ * @head: pointer to pointer of first node
+ * Return: void function.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+	listint_t *ptr_new;
+
+	new = *head;
+	if (*head == NULL || head == NULL)
+	{
+		return;
+	}
+	while (new != NULL)
+	{
+		ptr_new = new;
+		new = new->next;
+		free(ptr_new);
+	}
+	new = NULL;
+}
