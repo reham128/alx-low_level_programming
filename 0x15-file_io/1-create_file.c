2dfef3f219af46c68860d251fb065b9a6950a20e
Reham <r_nour30@yahoo.com>
2023-10-30 17:30:14 -0700

    2nd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	else if (text_content == NULL)
+	else if (text_content != NULL)
 	{
-		text_content = "";
 		for (strL = 0; text_content[strL]; strL++)
 			;
 	}
