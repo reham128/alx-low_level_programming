71a8a462966f6bc270e1ea3a4f3e97af3dd5c07d
Reham <r_nour30@yahoo.com>
2023-12-08 13:30:07 -0800

    fine node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function to fine nth node of dlistint_t linked list
+ * @head: pointer to the head node of the list
+ * @index: the index to find the wanted node
+ * Return: the nth node wanted, or null if fail
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int tmp = 0;
+
+	if (head != NULL)
+	{
+		if (tmp == index)
+		{
+			return (head);
+		}
+		tmp = tmp + 1;
+		head = head->next;
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (NULL);
+}
