a8925a8e47824df9e8bfca7ab34379bf033509f7
Reham <r_nour30@yahoo.com>
2024-04-11 16:11:18 -0700

    up

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t idx;
 	size_t step = sqrt(size);
 
-	if (array == NULL || size ==0)
+	if (array == NULL || size == 0)
 	{
 		return (-1);
 	}
